f7859bbf08f6a1b7e98034c31b8c721d6e06b2cd
Leonardo <leonida03.97@gmail.com>
2023-07-25 21:26:03 +0200

    added ppdl domain and problem, pnp actions

--- playground/main.py
+++
@@ -1,9 +0,0 @@
-from modim.scripts import *
-import os, sys
-pdir = os.getenv('PNP_HOME')
-sys.path.insert(0, pdir+'/PNPnaoqi/py')
-import pnp_cmd_naoqi
-from pnp_cmd_naoqi import *
-
-m = modim.Modim()
-m.home()
